c0568795c746b4eb3bfa0577d9392a23f25cdd71
piper-vision <hello@piper.vision>
2026-07-08 10:20:26 +0100

    update

--- testing/threejs/spaceman-shards/dist/src/main.js
+++
@@ -1,7 +0,0 @@
-import { App } from './core/App.js';
-
-const app = new App(document.querySelector('#scene'));
-app.start();
-
-// Handy console handle for live tuning; harmless in production.
-window.__app = app;
